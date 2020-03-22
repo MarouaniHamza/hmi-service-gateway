666df31c41364eda74c06b6523f48241abb2f79a
MarouaniHamza <hamza.marouenihamza@gmail.com>
2020-03-22 12:09:00 +0100

    Setup gateway micro-service

---
+++ src/main/java/com/hmi/gateway/HmiServiceGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.hmi.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmiServiceGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HmiServiceGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hmi/gateway/HmiServiceGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hmi.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HmiServiceGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
